af0866e6c56f4a4b84924b02c8fa620ffbb34c3b
Bin Zhang <binz@stripe.com>
2020-08-30 14:37:37 -0700

    search

--- popup.js
+++ popup.js
@@ -15,7 +15,7 @@ function currentTab() {
 }
 
 async function gotoTab(el) {
-  let tid = parseInt(el.target.id.substring(1))
+  let tid = parseInt(el.id.substring(1))
   let tab = await getTab(tid)
   let cw = await currentWin()
   if (cw.id != tab.windowId) {
@@ -43,14 +43,51 @@ async function getAllTabs() {
   return tabs
 }
 
+function searchTab(ev) {
+  if (ev.key === "Enter") {
+    selectFirstMatchedTab()
+    return
+  }
+
+  let kws = ev.target.value.toLocaleLowerCase().split(/ +/).filter(w => w.length > 0)
+  let links = document.querySelectorAll("a")
+  let matched = 0
+  for (let ln of links) {
+    let title = ln.innerText.toLocaleLowerCase()
+    let all = kws.length > 0
+    for (let kw of kws) {
+      if (title.indexOf(kw) < 0) {
+        all = false
+        break
+      }
+    }
+    if (all) {
+      matched++
+      ln.classList.add("matched")
+    } else {
+      ln.classList.remove("matched")
+    }
+  }
+
+  let s = matched > 0 ? `${matched}/${links.length}` : `${links.length}`
+  document.querySelector("#count").innerText = s
+}
+
+function selectFirstMatchedTab() {
+  let matched = document.querySelector(".matched")
+  if (matched) {
+    gotoTab(matched)
+  }
+}
+
 async function render() {
   function tabToLi(tab, i) {
     let li = document.createElement("li")
     li.innerHTML = `<a id="t${tab.id}" href="#">${i+1}. ${tab.title}</a>`
-    li.querySelector(`#t${tab.id}`).addEventListener("click", gotoTab)
+    li.querySelector(`#t${tab.id}`).addEventListener("click", ev => gotoTab(ev.target))
     return li
   }
-
+  
   let tabs = await getAllTabs()
   let left = document.querySelector("#left")
   let right = document.querySelector("#right")
@@ -59,6 +96,13 @@ async function render() {
     let div = i < half ? left : right
     div.appendChild(li)
   }
+
+  let search = document.querySelector("#search")
+  search.addEventListener("keyup", searchTab)
+  search.focus()
+
+  let count = document.querySelector("#count")
+  count.innerText = `${tabs.length}`
 }
 
 render()
